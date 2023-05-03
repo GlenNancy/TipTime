77a4dac23376d715809cd48e65827cebe191b2ff
glennancy <glennancy24@gmail.com>
2023-05-03 18:58:36 -0300

    Atualizando lógica

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
